3e03e02f54964d2b03f9370f8f275e3939e07d0d
dickylin <saputralin.dicky@gmail.com>
2016-10-06 15:02:56 +0700

    first commit

---
+++ Selasa_141110175_DickySaputralin/Latihan_1_1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Latihan_1_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Latihan_1_1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Information = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.Help = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.vScrollBar4 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar3 = new System.Windows.Forms.VScrollBar();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabControl1.SuspendLayout();
+            this.Information.SuspendLayout();
+            this.Help.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Information);
+            this.tabControl1.Controls.Add(this.Help);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(374, 333);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Information
+            // 
+            this.Information.Controls.Add(this.label2);
+            this.Information.Controls.Add(this.label1);
+            this.Information.Controls.Add(this.vScrollBar2);
+            this.Information.Controls.Add(this.vScrollBar1);
+            this.Information.ImageIndex = 1;
+            this.Information.Location = new System.Drawing.Point(4, 23);
+            this.Information.Name = "Information";
+            this.Information.Padding = new System.Windows.Forms.Padding(3);
+            this.Information.Size = new System.Drawing.Size(366, 306);
+            this.Information.TabIndex = 0;
+            this.Information.Text = "Information";
+            this.Information.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.LargeChange = 1;
+            this.vScrollBar2.Location = new System.Drawing.Point(286, 65);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(20, 157);
+            this.vScrollBar2.TabIndex = 1;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 1;
+            this.vScrollBar1.Location = new System.Drawing.Point(49, 65);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(20, 157);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // Help
+            // 
+            this.Help.Controls.Add(this.label6);
+            this.Help.Controls.Add(this.label5);
+            this.Help.Controls.Add(this.dateTimePicker1);
+            this.Help.Controls.Add(this.label4);
+            this.Help.Controls.Add(this.label3);
+            this.Help.Controls.Add(this.vScrollBar4);
+            this.Help.Controls.Add(this.vScrollBar3);
+            this.Help.ImageIndex = 0;
+            this.Help.Location = new System.Drawing.Point(4, 23);
+            this.Help.Name = "Help";
+            this.Help.Padding = new System.Windows.Forms.Padding(3);
+            this.Help.Size = new System.Drawing.Size(366, 306);
+            this.Help.TabIndex = 1;
+            this.Help.Text = "Help";
+            this.Help.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(303, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Max";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Min";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(83, 67);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(307, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // vScrollBar4
+            // 
+            this.vScrollBar4.LargeChange = 1;
+            this.vScrollBar4.Location = new System.Drawing.Point(301, 67);
+            this.vScrollBar4.Maximum = 20;
+            this.vScrollBar4.Name = "vScrollBar4";
+            this.vScrollBar4.Size = new System.Drawing.Size(31, 170);
+            this.vScrollBar4.TabIndex = 1;
+            this.vScrollBar4.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar4_Scroll);
+            // 
+            // vScrollBar3
+            // 
+            this.vScrollBar3.LargeChange = 1;
+            this.vScrollBar3.Location = new System.Drawing.Point(34, 67);
+            this.vScrollBar3.Maximum = 20;
+            this.vScrollBar3.Name = "vScrollBar3";
+            this.vScrollBar3.Size = new System.Drawing.Size(31, 170);
+            this.vScrollBar3.TabIndex = 0;
+            this.vScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar3_Scroll);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "help-icon-31.png");
+            this.imageList1.Images.SetKeyName(1, "Information-icon.png");
+            // 
+            // Latihan_1_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 358);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Latihan_1_1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Latihan_1_1";
+            this.Load += new System.EventHandler(this.Latihan_1_1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Information.ResumeLayout(false);
+            this.Information.PerformLayout();
+            this.Help.ResumeLayout(false);
+            this.Help.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Information;
+        private System.Windows.Forms.TabPage Help;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.VScrollBar vScrollBar4;
+        private System.Windows.Forms.VScrollBar vScrollBar3;
+    }
+}
+
---
+++ Selasa_141110175_DickySaputralin/Latihan_1_1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Latihan_1_1 : Form
+    {
+        public Latihan_1_1()
+        {
+            InitializeComponent();
+            vScrollBar1.Minimum = 0;
+            vScrollBar1.Maximum = 20;
+            vScrollBar2.Minimum = 0;
+            vScrollBar2.Maximum = 20;
+
+            vScrollBar1.Value = 0;
+            vScrollBar2.Value = 20;
+        }
+
+        private void vScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {
+            label3.Text = "" + vScrollBar3.Value.ToString();
+            int min = 0;
+            min = vScrollBar3.Value;
+            int tahun = DateTime.Now.Year - min;
+            dateTimePicker1.MinDate = new DateTime(tahun, DateTime.Now.Month, DateTime.Now.Day);
+        }
+
+        private void vScrollBar4_Scroll(object sender, ScrollEventArgs e)
+        {
+            label4.Text = "" + vScrollBar4.Value.ToString();
+            int maks = 0;
+            maks = vScrollBar4.Value;
+            int tahun = DateTime.Now.Year + maks;
+            dateTimePicker1.MaxDate = new DateTime(tahun, DateTime.Now.Month, DateTime.Now.Day);
+        }
+
+        private void Latihan_1_1_Load(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = Help;
+            label1.Text = "" + vScrollBar1.Value.ToString();
+            label2.Text = "" + vScrollBar2.Value.ToString();
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int tempbertambah;
+            tempbertambah = vScrollBar1.Value - 1;
+            int temp;
+            temp = vScrollBar1.Value;
+            int tempberkurang;
+            tempberkurang = vScrollBar1.Value + 1;
+            label1.Text = "" + vScrollBar1.Value.ToString();
+
+            //do
+            //{
+            //    if (vScrollBar1.Value > temp) {
+            //        vScrollBar2.Value -= 1;
+            //        label2.Text = "" + vScrollBar2.Value.ToString();
+            //    }
+            //    else if (vScrollBar1.Value < temp) {
+            //        vScrollBar2.Value += 1;
+            //        label2.Text = "" + vScrollBar2.Value.ToString();
+            //    }
+            //} while (vScrollBar1.Value > vScrollBar1.Minimum);
+
+            if (vScrollBar1.Value > vScrollBar1.Minimum)
+            {
+                if (vScrollBar2.Value > 0)
+                {
+                    vScrollBar2.Value -= 1;
+                    label2.Text = "" + vScrollBar2.Value.ToString();
+                }
+                else {
+                    vScrollBar2.Value += 0;
+                    label2.Text = "" + vScrollBar2.Value.ToString();
+                }
+
+            }
+            
+
+            else if (vScrollBar1.Value == vScrollBar1.Maximum)
+            {
+                vScrollBar2.Value += 1;
+                label2.Text = "" + vScrollBar2.Value.ToString();
+            }
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            label2.Text = "" + vScrollBar2.Value.ToString();
+        }
+    }
+}
