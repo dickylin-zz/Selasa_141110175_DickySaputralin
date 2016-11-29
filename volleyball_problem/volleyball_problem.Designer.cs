332d96c695c0bb6aef9f46be408b3662bf4094a7
dickylin <saputralin.dicky@gmail.com>
2016-11-29 11:46:36 +0700

    Sory telat pak - Volleyball

---
+++ volleyball_problem/volleyball_problem.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace volleyball_problem
+{
+    partial class volleyball_problem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(97, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(108, 220);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // volleyball_problem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "volleyball_problem";
+            this.Text = "volleyball_problem";
+            this.Load += new System.EventHandler(this.volleyball_problem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ volleyball_problem/volleyball_problem.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class volleyball_problem : Form
+    {
+        public volleyball_problem()
+        {
+            InitializeComponent();
+        }
+
+        private void volleyball_problem_Load(object sender, EventArgs e)
+        {
+
+        }
+        static int mod = 1000000007;
+        static private long kombinasi(int x, int y)
+        {
+            long hasil = 1;
+            for (int i = 0; i < y; i++)
+            {
+                hasil = hasil * (x - i) / (i + 1);
+            }
+            return hasil % mod;
+        }
+        static long pangkat(long x, long y)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            if (y == 1)
+            {
+                return x;
+            }
+            if (x == 0)
+            {
+                return 0;
+            }
+            long setengah = pangkat(x, y / 2);
+            if (y % 2 == 0)
+            {
+                return (setengah * setengah) % mod;
+            }
+            else
+            {
+                return (((setengah * setengah) % mod) * x) % mod;
+            }
+        }
+        static long jawab(int a, int b)
+        {
+            int tempSwap = 0;
+            if (a < b)
+            {
+                tempSwap = a;
+                a = b;
+                b = tempSwap;
+            }
+            if (a < 25)
+            {
+                return 0;
+            }
+            if (a == 25)
+            {
+                if (b >= 24)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return kombinasi(a + b - 1, b);
+                }
+            }
+            if (a - b != 2)
+            {
+                return 0;
+            }
+            else
+            {
+                return kombinasi(48, 24) * pangkat(2, a - 26) % mod;
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int text1 = Convert.ToInt32(textBox2.Text);
+            int text2 = Convert.ToInt32(textBox1.Text);
+            textBox3.Text = jawab(text1, text2).ToString();
+        }
+
+    }
+}
